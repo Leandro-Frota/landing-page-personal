650d9770cb96a700f814bc4b42eafe7ad1441d28
Leandro-Frota <leandromfrota@gmail.com>
2023-11-29 16:19:31 -0300

    Adicionei alteração de cor da img do perfil

--- src/script.js
+++ src/script.js
@@ -1,15 +1,23 @@
 const changeThemeBtn = document.querySelector("#dark-mode");
 
-var img = document.getElementById("profile_inf_personal_image");
-
-
 changeThemeBtn.addEventListener("change", function() { // aciona o evento ao mudar o seletor checkbox
     document.body.classList.toggle("dark"); // procura a classe do body do documento, aciona o toggle que avalia a classList, se não tiver a
     // classe informada, ele add, se tiver, remove.
 });
 
 
-changeThemeBtn.addEventListener("change", function() { // aciona o evento ao mudar o seletor checkbox
-    document.img.setAttribute('src','logo_avatar_dark.png');
+// let img = document.getElementById("profile_inf_personal_image");
 
-});
+// changeThemeBtn.addEventListener("check", function() { // aciona o evento ao mudar o seletor checkbox
+//    if(body.classList.match("dark")){
+//     img.src="assets/logo_avatar_dark.png";
+//    }else{
+//     img.src="assets/logo_avatar_ligth.png";
+//    }
+
+// });
+
+
+
+
+
